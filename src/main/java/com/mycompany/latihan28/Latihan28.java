3e73dc38ba132eb031f9ccda7d6cb01ef570ed74
farchatul07 <145920038+farchatul07@users.noreply.github.com>
2023-10-26 19:39:06 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/latihan28/Latihan28.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihan28;
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class Latihan28 {
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("===Program Ganti Kata===");
+
+        System.out.print("Masukkan kalimat: ");
+        String kalimat = scanner.nextLine();
+
+        System.out.print("Masukkan kata yang ingin diganti: ");
+        String kataYangInginDiganti = scanner.nextLine();
+
+        System.out.print("Masukkan kata pengganti: ");
+        String kataPengganti = scanner.nextLine();
+
+        // Menyimpan kalimat awal sebelum melakukan penggantian
+        String kalimatAwal = kalimat;
+
+        String kalimatBaru = kalimat.replace(kataYangInginDiganti, kataPengganti);
+
+        System.out.println("==HASIL FORMATTING==");
+        System.out.println("Kalimat awal: " + kalimatAwal);
+        System.out.println("Kalimat baru: " + kalimatBaru);
+    }
+    }
+
